b2dc189adca02ba90025d63bd5d3e9167f794f6c
AKSHAT PORWAL <mpakshat@gmail.com>
2024-06-19 18:11:45 +0530

    UI

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from gunicorn.http import message
+
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECREAT_KEY']= 'wfe&wocjpijj7674@#wvpep13pvpv'
-    message.MAX_REQUEST_LINE = 2**16 - 2
+    
     
 
     from .views import views
